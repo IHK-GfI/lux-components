dc6b03d00a2366e946caf1654ae118e98801ec40
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2021-12-02 14:13:30 +0100

    Butttons/Links wurden in seltenen Konstellationen zu spät dargestellt. Die beiden Services wurden auf BehaviorSubjects umgestellt.

--- src/app/modules/lux-layout/lux-app-footer/lux-app-footer.component.ts
+++ src/app/modules/lux-layout/lux-app-footer/lux-app-footer.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { ChangeDetectorRef, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { LuxAppService } from "../../lux-util/lux-app.service";
+import { LuxAppService } from '../../lux-util/lux-app.service';
 import { LuxMediaQueryObserverService } from '../../lux-util/lux-media-query-observer.service';
 import { LuxAppFooterButtonInfo } from './lux-app-footer-button-info';
 import { LuxAppFooterButtonService } from './lux-app-footer-button.service';
@@ -17,40 +17,51 @@ export class LuxAppFooterComponent implements OnInit, OnDestroy {
   @ViewChild('buttonMenu', { static: true }) buttonMenu: LuxMenuComponent;
 
   @Input() luxVersion: string;
-  @Input() luxAriaRoleFooterLabel = $localize `:@@luxc.app-footer.ariarolefooter:Fußzeilenbereich / Buttonbereich`;
+  @Input() luxAriaRoleFooterLabel = $localize`:@@luxc.app-footer.ariarolefooter:Fußzeilenbereich / Buttonbereich`;
 
   desktopView: boolean;
-  subscription: Subscription;
+  buttonInfos: LuxAppFooterButtonInfo[] = [];
+  linkInfos: LuxAppFooterLinkInfo[] = [];
+  subscriptions: Subscription[] = [];
 
   constructor(
     public buttonService: LuxAppFooterButtonService,
     private linkService: LuxAppFooterLinkService,
     private mediaObserver: LuxMediaQueryObserverService,
     private elementRef: ElementRef,
-    private appService: LuxAppService
+    private appService: LuxAppService,
+    private cdr: ChangeDetectorRef
   ) {
     this.appService.appFooterEl = elementRef.nativeElement;
   }
 
   ngOnInit() {
-    this.subscription = this.mediaObserver.getMediaQueryChangedAsObservable().subscribe(query => {
-      this.desktopView = this.mediaObserver.isSM() || this.mediaObserver.isMD() || this.mediaObserver.isLG() || this.mediaObserver.isXL();
-    });
+    this.subscriptions.push(
+      this.mediaObserver.getMediaQueryChangedAsObservable().subscribe((query) => {
+        this.desktopView = this.mediaObserver.isSM() || this.mediaObserver.isMD() || this.mediaObserver.isLG() || this.mediaObserver.isXL();
+      })
+    );
+
+    this.subscriptions.push(
+      this.buttonService.getButtonInfosAsObservable().subscribe((buttonInfos) => {
+        this.buttonInfos = buttonInfos;
+        this.cdr.detectChanges();
+      })
+    );
+
+    this.subscriptions.push(
+      this.linkService.getLinkInfosAsObservable().subscribe((linkInfos) => {
+        this.linkInfos = linkInfos;
+        this.cdr.detectChanges();
+      })
+    );
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-  getButtons(): LuxAppFooterButtonInfo[] {
-    return this.buttonService.buttonInfos;
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
   sendButtonCommand(cmd: string) {
     this.buttonService.sendButtonCommand(cmd);
-  }
-
-  getLinks(): LuxAppFooterLinkInfo[] {
-    return this.linkService.linkInfos;
   }
 }
