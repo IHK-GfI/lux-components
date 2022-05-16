b5c3fc5ea22bda3009fc517760a0b521b82e98ab
trowe <Pamela.Trowe@gfi.ihk.de>
2022-05-16 09:31:43 +0200

    LUX-Tile-Authentic Komponente und Demo

---
+++ src/app/demo/components-overview/tile-authentic-example/overview-example/overview-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lux-overview-example',
+  templateUrl: './overview-example.component.html',
+  styleUrls: ['./overview-example.component.scss']
+})
+export class OverviewExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/demo/components-overview/tile-authentic-example/tile-authentic-example.component.ts
+++ src/app/demo/components-overview/tile-authentic-example/tile-authentic-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LuxBadgeNotificationPosition, LuxBadgeNotificationSize } from '../../../modules/lux-directives/lux-badge-notification/lux-badge-notification.directive';
 import { logResult } from '../../example-base/example-base-util/example-base-helper';
 
 @Component({
@@ -10,12 +11,16 @@ export class TileAuthenticExampleComponent {
   showIcon = true;
   showOutputEvents = false;
 
-  showNotification;
-  counter = undefined;
-  counterCap = 20;
   label = 'Tile-Authentic';
   subTitle = 'Die neue Kachel mit einem Subtitle'
   log = logResult;
+
+  badgeContent = '';
+  badgeCap = 20;
+  badgePosition: LuxBadgeNotificationPosition = 'above after';
+  badgeSize: LuxBadgeNotificationSize = 'medium';
+  badgeColor = 'primary';
+  badgeDisabled = false;
 
   constructor() { }
 
