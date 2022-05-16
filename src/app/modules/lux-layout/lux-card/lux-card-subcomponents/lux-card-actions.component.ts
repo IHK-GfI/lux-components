9aa0389ca2465075e1ffedca73e29d24a610e10c
trowe <Pamela.Trowe@gfi.ihk.de>
2022-05-16 11:41:09 +0200

    lux-card - Ausrichtung lux-card-actions überarbeitet

--- src/app/modules/lux-layout/lux-card/lux-card-subcomponents/lux-card-actions.component.ts
+++ src/app/modules/lux-layout/lux-card/lux-card-subcomponents/lux-card-actions.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lux-card-actions',
-  template: '<div fxLayout="row" class="lux-flex-layout-gap-10"><ng-content></ng-content></div>'
+  template: '<div fxLayout="row" class="lux-flex-layout-gap-10" fxLayoutAlign="end center" ><ng-content></ng-content></div>'
 })
 export class LuxCardActionsComponent {
   constructor() {}
