fd92c48bbd29310a6cde9990dae89238fa31b26e
trowe <Pamela.Trowe@gfi.ihk.de>
2023-01-18 08:30:07 +0100

    Grid-Example für lux-tile-ac überarbeitet, Styling-Regeln für das Demo ergänzt

--- src/app/demo/components-overview/tile-authentic-example/overview-example/overview-example.component.ts
+++ src/app/demo/components-overview/tile-authentic-example/overview-example/overview-example.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lux-overview-example',
-  templateUrl: './overview-example.component.html'
+  templateUrl: './overview-example.component.html',
+  styleUrls: ['./overview-example.component.scss']
 })
 export class OverviewExampleComponent {
 
