e0a17b9588c62ece0992bf88c35d086a0aaf0738
GFI\Trowe <pamela.trowe@gfi.ihk.de>
2023-09-28 13:10:28 +0200

    Demo: Headingstruktur überarbeitet

--- src/app/demo/base/license-hint/license-hint.component.ts
+++ src/app/demo/base/license-hint/license-hint.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class LicenseHintComponent {
   licenceHintIcons = `
-### Icons
+## Icons
 
 In dieser Anwendung werden Streamline-Icons über das Github-Projekt [lux-components-icons-and-fonts](https://github.com/IHK-GfI/lux-components-icons-and-fonts) der IHK-GfI mbH eingebunden. Die verwendeten Icons laufen unter der Lizenz [Creative Commons 4.0 (CC BY 4.0)](https://creativecommons.org/licenses/by/4.0/). Der Urheber ist [Streamline Icons](https://www.streamlinehq.com/). Bezugsquelle: Core Line - Free (https://www.streamlinehq.com/icons/streamline-mini-line). Die Lizensierungsinformationen sind verfügbar unter https://www.streamlinehq.com/license-free. Die Icons aus dem o.g. Iconset wurden durch die IHK-GfI mbH nicht verändert. Im o.g. Github-Projekt wurden eigene Icons im selben Stil erstellt und der Sammlung unter gleicher Lizenz hinzugefügt.
 Bis zur Version 14.x.x werden ebenfalls die Font-Awesome sowie Material-Icons verwendet.
   `;
 
   licenceHintFonts = `
-### Fonts
+## Fonts
 
 Diese Anwendung verwendet die Schriftarten "Source Sans Pro" designed by Paul D. Hunt (lizensiert unter [SIL Open Font License V1.1](https://github.com/IHK-GfI/lux-components-icons-and-fonts/blob/master/assets/fonts/Source%20Sans%20Pro/SIL%20Open%20Font%20License%20V1.1.md)) sowie "BloggerSans" created by Sergiy Tkachenko (Lizenz: https://www.fontsquirrel.com/license/blogger-sans, lizensiert unter [Creative Commons 4.0 (CC BY 4.0)](https://creativecommons.org/licenses/by/4.0/)).
   `;
