71a3faa1884a8e2a5a8febfc7397829d904390c2
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2024-09-05 10:36:56 +0200

    Issue #447: MDC-Migration: @angular/material/dialog

--- src/app/modules/lux-popups/lux-dialog/lux-dialog-model/lux-dialog-ref.class.ts
+++ src/app/modules/lux-popups/lux-dialog/lux-dialog-model/lux-dialog-ref.class.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { Observable, ReplaySubject } from 'rxjs';
 import { take } from 'rxjs/operators';
 
