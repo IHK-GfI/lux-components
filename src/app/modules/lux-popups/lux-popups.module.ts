91cb9660363220b78b57cecb0557a31a97dffea8
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2024-09-27 09:22:34 +0200

    Issue #447: MDC-Migration: @angular/material/dialog

--- src/app/modules/lux-popups/lux-popups.module.ts
+++ src/app/modules/lux-popups/lux-popups.module.ts
@@ -2,7 +2,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { LuxIconModule } from '../lux-icon/lux-icon.module';
 import { LuxSnackbarComponent } from './lux-snackbar/lux-snackbar-component/lux-snackbar.component';
