8fb8c779e16e665b4d12c2f98ab1c9a6cf6cc839
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2025-05-13 09:57:23 +0200

    Build: Abhängigkeit "@ihk-gfi/lux-components-icons-and-fonts" auf die Version "1.10.0" erhöht.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,39 @@ import { CUSTOM_ELEMENTS_SCHEMA, DoBootstrap, Injector, NgModule } from '@angula
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DseComponent } from './demo/abstract/dse/dse.component';
+import { ImpressumComponent } from './demo/abstract/impressum/impressum.component';
+import { LicenseHintComponent } from './demo/base/license-hint/license-hint.component';
+import { BaselineModule } from './demo/baseline/baseline.module';
 import { MockLuxLookupService } from './demo/components-overview/lookup-examples/mock-lookup-service';
+import { ConfigurationModule } from './demo/configuration/configuration.module';
+import { ExampleBaseModule } from './demo/example-base/example-base.module';
 import { HomeModule } from './demo/home/home.module';
 import { LuxActionModule } from './modules/lux-action/lux-action.module';
+import { LuxBreadcrumbModule } from './modules/lux-breadcrumb/lux-breadcrumb.module';
+import { LuxCommonModule } from './modules/lux-common/lux-common.module';
+import { LuxComponentsConfigParameters } from './modules/lux-components-config/lux-components-config-parameters.interface';
+import { LuxComponentsConfigModule } from './modules/lux-components-config/lux-components-config.module';
 import { LuxDirectivesModule } from './modules/lux-directives/lux-directives.module';
+import { LuxErrorModule } from './modules/lux-error/lux-error.module';
 import { LuxFilePreviewModule } from './modules/lux-file-preview/lux-file-preview.module';
+import { LuxFormModule } from './modules/lux-form/lux-form.module';
 import { LuxLayoutModule } from './modules/lux-layout/lux-layout.module';
 import { LuxLookupService } from './modules/lux-lookup/lux-lookup-service/lux-lookup.service';
 import { LuxMarkdownModule } from './modules/lux-markdown/lux-markdown.module';
-import { LuxPopupsModule } from './modules/lux-popups/lux-popups.module';
-import { LuxConsoleService } from './modules/lux-util/lux-console.service';
-import { LuxComponentsConfigModule } from './modules/lux-components-config/lux-components-config.module';
-import { LuxComponentsConfigParameters } from './modules/lux-components-config/lux-components-config-parameters.interface';
-import { environment } from '../environments/environment';
-import { ConfigurationModule } from './demo/configuration/configuration.module';
-import { LuxFormModule } from './modules/lux-form/lux-form.module';
-import { BaselineModule } from './demo/baseline/baseline.module';
 import { LuxPipesModule } from './modules/lux-pipes/lux-pipes.module';
-import { ExampleBaseModule } from './demo/example-base/example-base.module';
-import { LuxCommonModule } from './modules/lux-common/lux-common.module';
-import { LuxErrorModule } from './modules/lux-error/lux-error.module';
-import 'hammerjs';
-import { DseComponent } from './demo/abstract/dse/dse.component';
-import { LicenseHintComponent } from './demo/base/license-hint/license-hint.component';
-import { ImpressumComponent } from './demo/abstract/impressum/impressum.component';
+import { LuxPopupsModule } from './modules/lux-popups/lux-popups.module';
 import { LuxTenantLogoModule } from './modules/lux-tenant-logo/lux-tenant-logo.module';
 import { LuxTourHintModule } from './modules/lux-tour-hint/lux-tour-hint.module';
-import { LuxBreadcrumbModule } from './modules/lux-breadcrumb/lux-breadcrumb.module';
+import { LuxConsoleService } from './modules/lux-util/lux-console.service';
 
 const myConfiguration: LuxComponentsConfigParameters = {
   generateLuxTagIds: environment.generateLuxTagIds,
-  iconBasePath: 'https://cdn.gfi.ihk.de/lux-components/icons-and-fonts/v1.8.0/',
+  iconBasePath: 'https://cdn.gfi.ihk.de/lux-components/icons-and-fonts/v1.10.0/',
   labelConfiguration: {
     allUppercase: true,
     notAppliedTo: ['lux-link', 'lux-menu-item', 'lux-side-nav-item', 'lux-tab', 'lux-step']
